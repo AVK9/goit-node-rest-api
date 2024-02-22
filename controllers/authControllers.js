263af41fb62cf6bd6b09da1bfd4ddc0d4fd34949
AlexKat <katsubo79@gmail.com>
2024-02-23 01:58:59 +0200

    Jimp

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,9 +5,10 @@ const { ctrlWrapper, HttpError } = require('../helpers/index.js');
 const gravatar = require('gravatar');
 const path = require('path');
 const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 const { SECRET_KEY } = process.env;
-const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
+const avatarsDir = path.join(__dirname, '..', 'public', 'avatars');
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -86,8 +87,14 @@ const avatars = async (req, res) => {
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join('avatars', filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  const avatarUrlPart = path.join('avatars', filename);
+  await User.findByIdAndUpdate(_id, { avatarUrlPart });
+  const avatarURL = path.join(__dirname, '..', 'public', 'avatars', filename);
+
+  Jimp.read(avatarURL, (err, avatar) => {
+    if (err) throw err;
+    avatar.resize(250, 250).quality(60).write(avatarURL);
+  });
 
   res.json({
     avatarURL,
