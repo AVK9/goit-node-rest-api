e8b6294c105ba57a67fc47651295f49cc1960090
AlexKat <katsubo79@gmail.com>
2024-02-27 03:04:46 +0200

    test

---
+++ controllers/__test__/authControllers.test.js
@@ -0,0 +1,73 @@
+const request = require('supertest');
+const app = require('../../app');
+const {
+  expect,
+  describe,
+  test,
+  beforeAll,
+  beforeEach,
+  afterEach,
+  afterAll,
+} = require('@jest/globals');
+
+const mongoose = require('mongoose');
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+describe('POST /users/login', () => {
+  beforeAll(() => console.log('BEFORE ALL'));
+  beforeEach(() => console.log('BEFORE EACH'));
+  afterEach(() => console.log('AFTER EACH'));
+  afterAll(() => console.log('AFTER ALL'));
+
+  test('test response status and token', async () => {
+    const testData = {
+      email: 'AlexKat@example.com',
+      password: 'examplepassword',
+    };
+
+    const res = await request(app).post('/users/login').send(testData);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+        user: expect.any(Object),
+      })
+    );
+  }, 5000);
+
+  test('should returns 401 error I', async () => {
+    const testData = {
+      email: 'AlexKat@example.com',
+      password: 'examplepassword2',
+    };
+    console.log(JSON.stringify(testData));
+    const res = await request(app).post('/users/login').send(testData);
+
+    expect(res.status).toBe(401);
+  }, 5000);
+
+  test('should returns 400 error II', async () => {
+    const testData = {
+      email: 'AlexKat@example.com',
+    };
+
+    const res = await request(app).post('/users/login').send(testData);
+
+    expect(res.statusCode).toBe(400);
+  });
+});
