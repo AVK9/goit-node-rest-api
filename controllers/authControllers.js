608b32e3cd98941d4d0b9eda4950ef32c83e51a4
AlexKat <katsubo79@gmail.com>
2024-02-29 15:43:23 +0200

    mail

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,17 +1,20 @@
 const { User } = require('../models/user.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { nanoid } = require('nanoid');
 const {
   ctrlWrapper,
   HttpError,
   userNameHandler,
-} = require('../helpers/index.js');
+  sendEmail,
+} = require('../helpers');
 const gravatar = require('gravatar');
 const path = require('path');
 const fs = require('fs/promises');
 const Jimp = require('jimp');
+const { updateSearchIndex } = require('../models/contact.js');
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 const avatarsDir = path.join(__dirname, '..', 'public', 'avatars');
 
 const register = async (req, res) => {
@@ -22,13 +25,22 @@ const register = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationCode = nanoid();
 
   const newUser = await User.create({
     ...req.body,
     name: userNameHandler(name),
     password: hashPassword,
     avatarURL,
+    verificationCode,
   });
+  const verifyEmail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<p>Якщо ви бажаєте підтвердити реєстрацію клікайте за посиланням - <a target="_blank" href="${BASE_URL}/users/verify/${verificationCode}">Click verify email</a></p>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
@@ -38,11 +50,50 @@ const register = async (req, res) => {
   });
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationCode } = req.params;
+  const user = await User.findOne({ verificationCode });
+  if (!user) {
+    throw HttpError(401, 'Email not found');
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationCode: '',
+  });
+  res.json({ message: 'Email verify succsess' });
+};
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, 'Email not found');
+  }
+  if (user.verify) {
+    throw HttpError(401, 'Email already verify');
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<p>Якщо ви бажаєте підтвердити реєстрацію клікайте за посиланням - <a target="_blank" href="${BASE_URL}/users/verify/${verificationCode}">Click verify email</a></p>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message: 'Verify email send success',
+  });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, 'Email or password is wrong');
+  }
+  if (!user.verify) {
+    throw HttpError(401, 'Email not verified');
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
@@ -107,10 +158,12 @@ const avatars = async (req, res) => {
 };
 
 module.exports = {
+  verifyEmail: ctrlWrapper(verifyEmail),
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   subscription: ctrlWrapper(subscription),
   avatars: ctrlWrapper(avatars),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
---
+++ helpers/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const { A_PASS } = process.env;
+
+const nodemailerConfig = {
+  host: 'mail.adm.tools',
+  port: 465,
+  //   secure: true,
+  auth: {
+    user: 'mail@alexk.site',
+    pass: A_PASS,
+  },
+};
+
+const sendEmail = async data => {
+  const transport = nodemailer.createTransport(nodemailerConfig);
+  const emailBady = { ...data, from: 'Alex Katsubo <mail@alexk.site>' };
+
+  transport
+    .sendMail(emailBady)
+    .then(() => console.log('Email send success'))
+    .catch(error => console.log(error.message));
+};
+
+module.exports = sendEmail;
